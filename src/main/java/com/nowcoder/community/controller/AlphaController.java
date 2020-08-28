1aff6803fe4504857cb286171530fd2f29d4c82d
liamlycoder <luyu.real@qq.com>
2020-08-28 12:27:15 +0800

    提交Spring MVC的学习示例

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -3,9 +3,15 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.service.AlphaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
 
 @Controller
 @RequestMapping("/alpha")
@@ -23,5 +29,109 @@ public class AlphaController {
     @ResponseBody
     public String getData() {
         return alphaService.find();
+    }
+
+    @RequestMapping("/http")
+    public void http(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 获取请求数据
+        System.out.println(request.getMethod());  // 请求方法
+        System.out.println(request.getServletPath());  // 请求路径
+        Enumeration<String> headerNames = request.getHeaderNames();  // 请求头名称
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(name + ": " + value);
+        }
+        System.out.println(request.getParameter("code"));  // 获取请求参数
+
+        // 返回响应数据
+        response.setContentType("text/html;charset=utf-8");
+        try (
+                PrintWriter writer = response.getWriter();  // 放到try的括号里面后面不用close
+                ) {
+            writer.write("<h1>牛客网</h1>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // GET请求的处理方式
+    // 访问路径：/students?current=1&limit=20
+    @RequestMapping(path="/students", method= RequestMethod.GET)
+    @ResponseBody
+    public String getStudents(
+            @RequestParam(name = "current", required = false, defaultValue = "1") int current,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) {
+
+        System.out.println(current);
+        System.out.println(limit);
+        return "some students";
+    }
+
+    // 访问路径：/student/123
+    @RequestMapping(path = "/student/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudent(@PathVariable("id") int id) {
+        System.out.println(id);
+        return "One Student";
+    }
+
+
+    // POST请求方式
+    @RequestMapping(path = "/student", method = RequestMethod.POST)
+    @ResponseBody
+    public String saveStudent(String name, String age) {
+        System.out.println(name);
+        System.out.println(age);
+        return "success";
+    }
+
+    // 响应html数据
+    @RequestMapping(path = "/teacher", method = RequestMethod.GET)
+    public ModelAndView getTeacher() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name", "张三");
+        modelAndView.addObject("age", 99);
+        modelAndView.setViewName("/demo/view");  // 指的是view.html  后缀省略不写
+        return modelAndView;
+    }
+
+    @RequestMapping(path = "/school", method = RequestMethod.GET)
+    public String getSchool(Model model) {
+        model.addAttribute("name", "北京大学");
+        model.addAttribute("age", 80);
+        return "/demo/view";
+    }
+
+    // 响应json数据（异步请求）
+    // Java对象 --> json --> JS对象
+    @RequestMapping(path = "/emp", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> getEmp() {
+        Map<String, Object> emp = new HashMap<>();
+        emp.put("name", "张三");
+        emp.put("age", 19);
+        emp.put("salary", 19999);
+        return emp;
+    }
+
+    @RequestMapping(path = "/emps", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Map<String, Object>> getEmps() {
+        List<Map<String, Object>> list = new ArrayList<>();
+        Map<String, Object> emp = new HashMap<>();
+        emp.put("name", "张三");
+        emp.put("age", 19);
+        emp.put("salary", 1999.9);
+
+        Map<String, Object> emp2 = new HashMap<>();
+        emp2.put("name", "李四");
+        emp2.put("age", 191);
+        emp2.put("salary", 199.99);
+
+        list.add(emp);
+        list.add(emp2);
+
+        return list;
     }
 }
