23848697f038acd4d294634b7ffdebd106f365d1
liamlycoder <luyu.real@qq.com>
2020-09-01 16:37:00 +0800

    提交统一异常处理的代码

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -5,9 +5,7 @@ import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.DiscussPostService;
 import com.nowcoder.community.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,10 +19,10 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
-    @Autowired
+    @Resource
     private DiscussPostService discussPostService;
 
-    @Autowired
+    @Resource
     private UserService userService;
 
 
@@ -34,6 +32,9 @@ public class HomeController {
         // 所以,在thymeleaf中可以直接访问Page对象中的数据.
         page.setRows(discussPostService.findDiscussPostRows(0));
         page.setPath("/index");
+
+        // 人为制造错误
+//        int a = 1/0;
 
         List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
         List<Map<String, Object>> discussPosts = new ArrayList<>();
@@ -48,5 +49,10 @@ public class HomeController {
         }
         model.addAttribute("discussPosts", discussPosts);
         return "/index";
+    }
+
+    @RequestMapping(path = "/error", method = RequestMethod.GET)
+    public String getErrorPage() {
+        return "/error/500";
     }
 }
