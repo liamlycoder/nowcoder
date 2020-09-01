e25552f126858fa5b0b03b28d3203273ebd7e218
liamlycoder <luyu.real@qq.com>
2020-09-01 12:27:33 +0800

    提交显示帖子详情页的功能代码

--- src/main/java/com/nowcoder/community/controller/DiscussPostController.java
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -3,9 +3,12 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +21,9 @@ import java.util.Date;
 class DiscussPostController {
     @Resource
     private DiscussPostService discussPostService;
+
+    @Resource
+    private UserService userService;
 
     @Resource
     private HostHolder hostHolder;
@@ -41,5 +47,17 @@ class DiscussPostController {
         return CommunityUtil.getJSONString(0, "发布成功!");
     }
 
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        // 获取帖子
+        DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", post);
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user", user);
+
+        return "/site/discuss-detail";
+
+
+    }
 }
 
