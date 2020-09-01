652d937e1879ef41febafae0567616b8cdc2a310
liamlycoder <luyu.real@qq.com>
2020-09-01 13:42:40 +0800

    提交spring boot事务管理的示例

--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -1,20 +1,107 @@
 package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.AlphaDao;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
+import java.util.Date;
 
 @Service
 public class AlphaService {
     @Resource
     private AlphaDao alphaDao;
 
+    @Resource
+    private UserMapper userMapper;
+
+    @Resource
+    private DiscussPostMapper discussPostMapper;
+
+    @Resource
+    private TransactionTemplate transactionTemplate;
+
+
     public String find() {
         return alphaDao.select();
     }
+
+    /*
+    REQUIRED: 支持当前事务（外部事务），如果不存在则创建新事务。
+    REQUIRES_NEW: 创建一个新的事务，并且暂停当前事务(外部事务)。
+    NESTED: 如果当前存在事务（外部事务），则嵌套在该事务中执行(独立的提交和回滚)，否则就会和REQUIRED一样。
+     */
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public Object save1() {
+        // 新增用户
+        User user = new User();
+        user.setUsername("alpha");
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+        user.setEmail("alpha@qq.com");
+        user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        // 新增帖子
+        DiscussPost discussPost = new DiscussPost();
+        discussPost.setUserId(user.getId());
+        discussPost.setTitle("hello");
+        discussPost.setContent("新人报到");
+        discussPost.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(discussPost);
+
+        // 人为制造错误
+        Integer.valueOf("abc");
+
+        return "ok";
+    }
+
+    // 编程式事务示例
+    public Object save2() {
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        return transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus transactionStatus) {
+                // 新增用户
+                User user = new User();
+                user.setUsername("alpha2");
+                user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+                user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+                user.setEmail("alpha2@qq.com");
+                user.setHeaderUrl("http://image.nowcoder.com/head/999t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUser(user);
+
+                // 新增帖子
+                DiscussPost discussPost = new DiscussPost();
+                discussPost.setUserId(user.getId());
+                discussPost.setTitle("Hello NIhao");
+                discussPost.setContent("我是新人");
+                discussPost.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(discussPost);
+
+                // 人为制造错误
+                Integer.valueOf("abc");
+
+                return "ok";
+            }
+        });
+
+    }
+
 
 }
